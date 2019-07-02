356a239b3a84054b192b58e05f3f04930816891f
varannchik <varannchik@gmail.com>
2019-07-02 20:23:17 +0300

    commit

--- main.js
+++ main.js
@@ -4,18 +4,21 @@ let ost=document.querySelector('.ostatok');
 let pr=document.querySelector('.price');
 let add=document.querySelector('.add');
 let sale=document.querySelector('.sale');
-
+let myCount=0;
+let b=0;
 function Prod(name,price,total){ 
     this.name=name;
     this.price=price;
     this.total=total;
     const self=this;
-    btn.addEventListener('click', function() {
+
+    btn.addEventListener('click', function() {        
         this.name=document.querySelector('#select').value;
-        this.count=document.querySelector('.count').value;                                    
+        this.count=document.querySelector('.count').value;
+        myCount=this.count;                                            
         if(this.name==self.name){
             pr.value=self.price;
-            let b=self.total-this.count;                        
+            b=self.total-this.count;                        
             if(b<3){
                 ost.innerHTML='совсем мало'+' '+b;                
                 }if(b<0){
@@ -27,23 +30,65 @@ function Prod(name,price,total){
         } 
     });
     sale.addEventListener('click',function(){        
-        self.total=self.total-self.count;
-        console.log(self.total);            
+        self.total=+b;
+        console.log(self.total);                    
     });
     add.addEventListener('click',function(){        
         let count_add=document.querySelector('.count_add').value;
-        self.total=self.total-self.count + count_add;               
+        self.total=+b + +count_add;
+        console.log(self.total);
     });    
 }
 
-let prod1=new Prod('Молоко','30','100');
-let prod2=new Prod('Хлеб','15','30');
-let prod3=new Prod('Сыр','50','70');
-let prod4=new Prod('Изюм','25','25');
-let prod5=new Prod('Яйца','12','15');
-let prod6=new Prod('Огурцы','7','10');
+// let prod1=new Prod('Молоко','30','100');
+// let prod2=new Prod('Хлеб','15','30');
+// let prod3=new Prod('Сыр','50','70');
+// let prod4=new Prod('Изюм','25','25');
+// let prod5=new Prod('Яйца','12','15');
+// let prod6=new Prod('Огурцы','7','10');
 
 
 
+const items = [
+    {
+        name: 'Молоко',
+        price: 30,
+        total: 100,
+    },
+    {
+        name: 'Хлеб',
+        price: 15,
+        total: 30,
+    },
+    {
+        name: 'Сыр',
+        price: 50,
+        total: 70,
+    },
+    {
+        name: 'Изюм',
+        price: 25,
+        total: 25,
+    },
+    {
+        name: 'Яйца',
+        price: 12,
+        total: 15,
+    },
+    {
+        name: 'Огурцы',
+        price: 7,
+        total: 10,
+    },    
+]
+
+let resultName = items.map(({ name }) => name);
+let resultPrice = items.map(({ price }) => price);
+let resultTotal = items.map(({ total }) => total);
+
+let prod={};
+for(let i=0; i<resultName.length; i++){
+    prod[i]=new Prod(resultName[i], resultPrice[i], resultTotal[i]);      
+}
 
 
